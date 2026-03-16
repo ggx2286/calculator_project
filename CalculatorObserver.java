82b0e6573e415aebde551aabeb91df3614362fbf
ggx2286 <x672614@gmail.com>
2026-03-16 06:08:00 +0300

    Delete CalculatorObserver.java

--- CalculatorObserver.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-
-/**
- * Observer interface for the Observer Pattern.
- * The View implements this to receive updates from the Model.
- */
-public interface CalculatorObserver {
-    void update(String displayValue);
-}
